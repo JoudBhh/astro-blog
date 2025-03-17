5ce06d82ac63fe89730100bdd8fa57ad7167bc54
Joud <joud.hajhmida@gmail.com>
2025-03-18 00:46:14 +0100

    declare constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION = 'Articles, stories and tutorials from Tech Peaople';
+export const HOMEPAGE_ARTCLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
